ff82b8f3508b4a7d3ee45695c64c7715cac1aedd
Carla Holcomb <carla.programmer@gmail.com>
2021-02-19 19:23:31 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,16 +34,18 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
-  get '/articles/:id/edit' do
-    @article = Article.find(params[:id])
-    erb :edit
-  end
-
-  patch '/articles/:id' do
-    @article = Article.find(params[:id])
-      @article.update(params[:article])
-      redirect to "/articles/#{ @article.id }"
-  end
+  # edit
+   get "/articles/:id/edit" do
+     @article = Article.find(params[:id])
+     erb :edit
+   end
+ 
+   # update
+   patch "/articles/:id" do
+     @article = Article.find(params[:id])
+     @article.update(params[:article])
+     redirect to "/articles/#{ @article.id }"
+   end
 
   delete '/articles/:id/delete' do
     Article.destroy(params[:id])
